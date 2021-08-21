6e9acf555605bfa636f78fda8e5e79ed57e0ad8c
wasaab <fake@fake.email.com>
2021-08-21 09:37:10 -0400

    custom-queries.js added with showsByDate query that includes reviews in the response. serve-prod script now waits for graphql endpoint to be up with wait-on dep.

---
+++ src/graphql/custom-queries.js
@@ -0,0 +1,42 @@
+export const showsByDate = /* GraphQL */ `
+  query ShowsByDate(
+    $type: ShowType
+    $createdAt: ModelStringKeyConditionInput
+    $sortDirection: ModelSortDirection
+    $filter: ModelShowFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    showsByDate(
+      type: $type
+      createdAt: $createdAt
+      sortDirection: $sortDirection
+      filter: $filter
+      limit: $limit
+      nextToken: $nextToken
+    ) {
+      items {
+        id
+        title
+        type
+        rating
+        img
+        year
+        description
+        imdbRating
+        rtRating
+        reviews {
+          items {
+            showId
+            userId
+            rating
+          }
+          nextToken
+        }
+        createdAt
+        updatedAt
+      }
+      nextToken
+    }
+  }
+`;
