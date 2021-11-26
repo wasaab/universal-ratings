5bdf22bb86d113c897cbea4bb7537b9ac287c0ae
wasaab <fake@fake.email.com>
2021-11-26 09:51:59 -0500

    * discord webhook posted when shows are added. * showIdx now accurate for non-trending shows while on HOME view.

--- amplify/backend/function/ShowAddedDiscordWebhookPublisher/src/index.js
+++ amplify/backend/function/ShowAddedDiscordWebhookPublisher/src/index.js
@@ -4,7 +4,9 @@
 	WEBHOOK_URL
 Amplify Params - DO NOT EDIT */
 
-function buildRatingDesc({ imdbRating, rtRating }) {
+const axios = require('axios');
+
+function buildRatingDesc(imdbRating, rtRating) {
 	let ratingsLabel = '';
 
 	if (rtRating) {
@@ -24,29 +26,44 @@ const typeToIcon = {
 
 function buildRatingEmbed(show) {
 	return {
-    title: '⭐'.repeat(show.rating),
-		description: buildRatingDesc(show),
+    title: '⭐'.repeat(show.rating.N),
+		description: buildRatingDesc(show.imdbRating.N, show.rtRating.N),
 		author: {
-			icon_url: typeToIcon[show.type],
-			name: show.title,
+			icon_url: typeToIcon[show.type.S],
+			name: show.title.S,
       url: 'https://ratings.kent.codes'
 		},
-		thumbnail: { url: show.img },
-		footer: {
-			icon_url: 'https://i.imgur.com/9N0ZGSD.png',
-      text: "owner"
-		}
+		thumbnail: { url: show.img.S }
 	};
 }
 
-exports.handler = (event) => {
-  console.log('webhook url: ', process.env.WEBHOOK_URL);
-  console.log(JSON.stringify(event, null, 2));
+function postToWebhook(embed, showId) {
+  return axios.post(
+    process.env.WEBHOOK_URL,
+    { embeds: [embed] },
+    { headers: { 'content-type': 'application/json' } }
+  )
+    .then(() => `Webhook post succeeded for show: ${showId}`)
+    .catch((error) => {
+      console.error('Failed webhook post: ', error);
 
-  event.Records.forEach((record) => {
-    console.log(record.eventID);
-    console.log(record.eventName);
-    console.log('DynamoDB Record: %j', record.dynamodb);
-  });
-  return Promise.resolve('Successfully processed DynamoDB record');
+      return error.message;
+    });
+}
+
+exports.handler = (event) => {
+  console.log('posting to webhook url: ', process.env.WEBHOOK_URL);
+
+  const showsAdded = event.Records
+    .filter(({ eventName }) => eventName === 'INSERT')
+    .map((record) => {
+      console.log('eventId: ', record.eventID);
+      console.log('show added: %j', record.dynamodb);
+
+      const embed = buildRatingEmbed(record.dynamodb.NewImage);
+
+      return postToWebhook(embed, record.dynamodb.Keys.id.S);
+    });
+
+  return Promise.all(showsAdded);
 };
