924953bc000843a3252867580135cfb9d853d906
wasaab <fake@fake.email.com>
2022-02-01 20:52:18 -0500

    * Imports rearranged for consistency across files. * obselete svg todos removed. * 4 --> 2 space indents in TitleSearchBar & AlgoliaApiClient.

--- src/client/AlgoliaApiClient.js
+++ src/client/AlgoliaApiClient.js
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 export default class AlgoliaApiClient {
   constructor(appId, searchApiKey) {
-      this.appId = appId;
-      this.reqConfig = {
-          headers: {
-              'X-Algolia-Application-Id': appId,
-              'X-Algolia-API-Key': searchApiKey
-          }
-      };
-      this.fallbacks = this.randomizeFallbacks();
+    this.appId = appId;
+    this.reqConfig = {
+      headers: {
+        'X-Algolia-Application-Id': appId,
+        'X-Algolia-API-Key': searchApiKey
+      }
+    };
+    this.fallbacks = this.randomizeFallbacks();
   }
 
   /**
@@ -20,15 +20,15 @@ export default class AlgoliaApiClient {
    * @returns {number[]} the randomized fallback host postfixes
    */
   randomizeFallbacks() {
-      const fallbacks = [1,2,3,4];
+    const fallbacks = [1, 2, 3, 4];
 
-      for (let i = fallbacks.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
+    for (let i = fallbacks.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
 
-          [fallbacks[i], fallbacks[j]] = [fallbacks[j], fallbacks[i]];
-      }
+      [fallbacks[i], fallbacks[j]] = [fallbacks[j], fallbacks[i]];
+    }
 
-      return fallbacks;
+    return fallbacks;
   }
 
   /**
@@ -38,7 +38,7 @@ export default class AlgoliaApiClient {
    * @returns {string|number} the fallback host postfix
    */
   getFallback(retriesRemaining) {
-      return retriesRemaining === this.fallbacks.length ? '' : this.fallbacks[retriesRemaining];
+    return retriesRemaining === this.fallbacks.length ? '' : this.fallbacks[retriesRemaining];
   }
 
   /**
@@ -48,9 +48,9 @@ export default class AlgoliaApiClient {
    * @returns {string} the request url
    */
   buildUrl(retriesRemaining) {
-      const fallback = this.getFallback(retriesRemaining);
+    const fallback = this.getFallback(retriesRemaining);
 
-      return `https://${this.appId}${fallback}-dsn.algolia.net/1/indexes/show/query`;
+    return `https://${this.appId}${fallback}-dsn.algolia.net/1/indexes/show/query`;
   }
 
   /**
@@ -62,15 +62,15 @@ export default class AlgoliaApiClient {
    * @returns {Promise<Object[]>} matching shows
    */
   async search(query, retriesRemaining = this.fallbacks.length) {
-      try {
-          const { data } = await axios.post(this.buildUrl(retriesRemaining), { query }, this.reqConfig);
+    try {
+      const { data } = await axios.post(this.buildUrl(retriesRemaining), { query }, this.reqConfig);
 
-          return data;
-      } catch (err) {
-          if (!retriesRemaining || axios.isCancel(err)) { throw err; }
+      return data;
+    } catch (err) {
+      if (!retriesRemaining || axios.isCancel(err)) { throw err; }
 
-          return this.search(query, retriesRemaining - 1);
-      }
+      return this.search(query, retriesRemaining - 1);
+    }
   }
 
   /**
@@ -81,8 +81,8 @@ export default class AlgoliaApiClient {
    * @returns {Promise<Object[]>} matching shows
    */
   searchByTitle(title, cancelToken) {
-      this.reqConfig.cancelToken = cancelToken;
+    this.reqConfig.cancelToken = cancelToken;
 
-      return this.search(title);
+    return this.search(title);
   }
 }
