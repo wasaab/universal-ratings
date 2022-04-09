04c562bc7e3875ede227887cd837463269d2fc12
wasaab <fake@fake.email.com>
2022-04-09 19:44:45 -0400

    external ratings and streaming providers updated daily to prevent stale data

---
+++ amplify/backend/function/UpdateShowsMetadata/src/client/AlgoliaApiClient.mjs
@@ -0,0 +1,81 @@
+import axios from 'axios';
+
+export default class AlgoliaApiClient {
+  constructor(appId, searchApiKey) {
+    this.appId = appId;
+    this.reqConfig = {
+      headers: {
+        'X-Algolia-Application-Id': appId,
+        'X-Algolia-API-Key': searchApiKey
+      }
+    };
+    this.fallbacks = this.randomizeFallbacks();
+  }
+
+  /**
+   * Randomize the algolia fallback hosts 1-4, to distribute the load
+   * between clients when requests fail.
+   * Based on Fisher–Yates Shuffle.
+   *
+   * @returns {number[]} the randomized fallback host postfixes
+   */
+  randomizeFallbacks() {
+    const fallbacks = [1, 2, 3, 4];
+
+    for (let i = fallbacks.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+
+      [fallbacks[i], fallbacks[j]] = [fallbacks[j], fallbacks[i]];
+    }
+
+    return fallbacks;
+  }
+
+  /**
+   * Builds the host of the algolia search url.
+   * If retrying a failed request, the next fallback host is used.
+   * 
+   * @param {number} retriesRemaining - the number of request retries remaining
+   * @returns {string} the host of the search url
+   */
+   buildHost(retriesRemaining) {
+    const hostPostfix = retriesRemaining === this.fallbacks.length
+      ? 'dsn.algolia.net'
+      : `${this.fallbacks[retriesRemaining]}.algolianet.com`;
+
+    return `${this.appId}-${hostPostfix}`;
+  }
+
+  /**
+   * Builds the request url for algolia search with the provided fallback host postfix.
+   *
+   * @param {number} retriesRemaining - the number of req retries remaining
+   * @returns {string} the request url
+   */
+  buildUrl(retriesRemaining) {
+    return `https://${this.buildHost(retriesRemaining)}/1/indexes/show/query`;
+  }
+
+  /**
+   * Searches for shows matching the provided query.
+   * Retries on failure, using a different fallback host each time.
+   *
+   * @param {Object} params - the request parameters
+   * @param {string} params.query - the query to search for
+   * @param {number=} params.hitsPerPage - show hits per page (limit 1000)
+   * @param {string[]=} params.attributesToRetrieve - the show attributes to retrieve
+   * @param {number=} retriesRemaining - the number of req retries remaining
+   * @returns {Promise<Object[]>} matching shows
+   */
+  async search(params, retriesRemaining = this.fallbacks.length) {
+    try {
+      const { data } = await axios.post(this.buildUrl(retriesRemaining), params, this.reqConfig);
+
+      return data;
+    } catch (err) {
+      if (!retriesRemaining || axios.isCancel(err)) { throw err; }
+
+      return this.search(params, retriesRemaining - 1);
+    }
+  }
+}
---
+++ amplify/backend/function/UpdateShowsMetadata/src/client/index.mjs
@@ -0,0 +1,3 @@
+export { default as OmdbApiClient } from './OmdbApiClient.mjs';
+export { default as TmdbApiClient } from './TmdbApiClient.mjs';
+export { default as AlgoliaApiClient } from './AlgoliaApiClient.mjs';
